eade4c4799ed2bc6593bf10cd8c2e237a4c71945
Alexis Demetriou <ademetriou5@uclan.ac.uk>
2023-03-17 16:24:24 +0200

    Help display message

--- js/background.js
+++ js/background.js
@@ -8,3 +8,13 @@ video.addEventListener("ended", function() {
     video.currentTime = 0;
     video.play();
 });
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function hideModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
